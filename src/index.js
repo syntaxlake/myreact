32b3a665ecaf04874ace26380f69ecd62188fe3c
syntaxlake <tephrike@nate.com>
2023-04-20 13:09:18 +0900

    react01

--- src/index.js
+++ src/index.js
@@ -15,3 +15,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//App-> react 외 태그는 무조건 소문자. 
