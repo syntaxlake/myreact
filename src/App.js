fb0ae27e40e6cb068a4239b60adc9d15765c0ecb
syntaxlake <tephrike@nate.com>
2023-04-21 11:35:14 +0900

    react 02

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+// import Hello from './01/Hello';
+
 import './App.css';
+import MyClock from './02/MyClock';
 
 function App() {
   return (                                              // 사용자정의함수는 반드시 return이 들어가야함. 최소 return 1개. return에는 tag하나만 있어야함. (자식들 포함 div 하나 있어야 함. root div에 1개만 붙을 수 있음.)
-    <div className="App">                            
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <MyClock/> 
+    );
 }
 
 export default App;
-
-//className = html css의 class 기능
