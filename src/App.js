c015bd26289a3e2a371efa4ea27b59395c805e9e
syntaxlake <tephrike@nate.com>
2023-05-09 14:05:44 +0900

    react 081

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@
 // import Hello from './01/Hello';
 
 //import './App.css';
-//import Box from './03/Box';
+//import Boxcopy from './03/Box copy';
 //import MyClock from './02/MyClock';
 //import MyDiv from './04/MyDiv' ;
 //import Frcst from './05/Frcst';
-import Taccident from "./06/Taccident";
+//import MyRef from "./07/MyRef";
+import Gallery from './08/Gallery';
 
 function App() {
   return (                                              // 사용자정의함수는 반드시 return이 들어가야함. 최소 return 1개. return에는 tag하나만 있어야함. (자식들 포함 div 하나 있어야 함. root div에 1개만 붙을 수 있음.)
       // <MyClock/> 
       // <MyClock/>
-      // <Box/>
+      // <Boxcopy/>
       // <MyDiv />
-      <Taccident />
+      // <MyRef/>
+      <Gallery/>
     );
 }
 
