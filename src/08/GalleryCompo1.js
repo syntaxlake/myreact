64405c1b85bbbe8f93671620560580ecd03bf278
syntaxlake <tephrike@nate.com>
2023-05-15 23:52:55 +0900

    react 15

--- src/08/GalleryCompo1.js
+++ src/08/GalleryCompo1.js
@@ -1,23 +1,38 @@
 import React from "react";
+import styles from "./GalleryCompo1.module.css";
+
 const GalleryCompo1 = ({ tour }) => {
-  return (
-    <div>
-      {tour.map((item) => {
-        console.log(item.galTitle, item.galWebImageUrl, item.galPhotographyLocation);
-        // Replace "http://" -> "https://" (http시 방화벽 발동)
-        const imageUrl = item.galWebImageUrl.replace("http://", "https://");
-        return (
-          <div key={item.galTitle}>
-            <h2>{item.galTitle}</h2>
-            <div>
-              <img src={imageUrl} alt={item.galTitle} />
+  const gridItems = [];
+  const columns = 2; 
+  const rows = Math.ceil(tour.length / columns); 
+
+  for (let i = 0; i < rows; i++) {
+    const startIndex = i * columns;
+    const endIndex = startIndex + columns;
+    const rowItems = tour.slice(startIndex, endIndex);
+
+    const row = (
+      <div className={styles.row} key={i}>
+        {rowItems.map((item) => {
+          console.log(item.galTitle, item.galWebImageUrl, item.galPhotographyLocation);
+          const imageUrl = item.galWebImageUrl.replace("http://", "https://");
+          return (
+            <div className={styles.item} key={item.galTitle}>
+              <h2>{item.galTitle}</h2>
+              <div>
+                <img src={imageUrl} alt={item.galTitle} />
+              </div>
+              <p>{item.galPhotographyLocation}</p>
             </div>
-            <p>{item.galPhotographyLocation}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+          );
+        })}
+      </div>
+    );
+
+    gridItems.push(row);
+  }
+
+  return <div className={styles.grid}>{gridItems}</div>;
 };
 
 export default GalleryCompo1;
